ae02fefced6abc6c03b96e1c3d8edc2cdd4f77a1
Vladimir <108664254+boomervoo@users.noreply.github.com>
2026-05-27 13:07:37 +0700

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+    output: 'export',
+
+    basePath: isProd ? '/portfolio' : '',
+    assetPrefix: isProd ? '/portfolio/' : '',
+
+    images: {
+        unoptimized: true,
+    },
+
     turbopack: {
         rules: {
-            "*.svg": {
-                loaders: ["@svgr/webpack"],
-                as: "*.js",
+            '*.svg': {
+                loaders: ['@svgr/webpack'],
+                as: '*.js',
             },
         },
     },
