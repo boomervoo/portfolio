6d29871ecfd4bdb1ef96aacee95773ac32b01197
Vladimir <108664254+boomervoo@users.noreply.github.com>
2026-05-31 23:16:59 +0700

    fix [2]

--- next.config.ts
+++ next.config.ts
@@ -6,25 +6,27 @@ const isProd = process.env.NODE_ENV === "production";
 const basePath = isProd ? `/${repoName}` : "";
 
 const nextConfig: NextConfig = {
-    basePath,
-    assetPrefix: isProd ? `${basePath}/` : "",
-  
-    images: {
-      unoptimized: true,
+  output: "export",
+
+  basePath,
+  assetPrefix: isProd ? `${basePath}/` : "",
+
+  images: {
+    unoptimized: true,
+  },
+
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
+
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
     },
-  
-    env: {
-      NEXT_PUBLIC_BASE_PATH: basePath,
-    },
-    
-    turbopack: {
-        rules: {
-            "*.svg": {
-                loaders: ["@svgr/webpack"],
-                as: "*.js",
-            },
-        },
-    },
+  },
 };
 
 export default nextConfig;
